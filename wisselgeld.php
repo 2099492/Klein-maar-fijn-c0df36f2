60dbf081a9148422009b6f771a037a885f24b7fe
2099492 <2099492@talnet.com>
2019-12-04 14:04:29 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,36 @@
+<?php
+$input = ($argv[1]);
+
+
+$data = array(50,20,10,5,2,1);
+$data2 = array(50,20,10,5);
+
+$geld = intval($input);
+$cent = $input - $geld;
+$cent = intval(round($cent * 100));
+
+foreach ($data as $value) {
+
+  $wissel = floor($input / $value);
+
+  if ($wissel >= 1) {
+
+    $input = $input - ($value * $wissel);
+
+
+    echo $wissel . " x " . $value . " euro" . PHP_EOL;
+  }
+
+
+}
+foreach ($data2 as $value) {
+    $restcent = floor($cent / $value);
+    if ($restcent >= 1) {
+        $cent = $cent - ($value * $restcent);
+        echo $restcent . " x " . $value . " cent" . PHP_EOL;
+    }
+}
+// $input = 3.75;
+// $cent = $input - (int)$input;
+// echo $cent;
+?>
